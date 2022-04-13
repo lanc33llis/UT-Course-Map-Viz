7dcdd941d4924537cf07f22e01573c63a0c21a13
Lance Ellis <scdelance@gmail.com>
2022-04-12 21:41:33 -0500

    Revert "filter update fixes zoom and centers camera"
    This reverts commit ac10f76234df52b82e71eb0b19ee6ae120f0ce6a.

--- pages/components/ForceGraph.jsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-import { ForceGraph2D } from 'react-force-graph'
-
-const WrappedForceGraph = ({fgRef, ...props}) => {
-  return <ForceGraph2D {...props} ref={fgRef}></ForceGraph2D>
-}
-
-export default WrappedForceGraph
